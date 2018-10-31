34bd1cc50b6e65bb87ee4c05d0b9d1c67a773189
emmaTu <emma.n.tuovinen@gmail.com>
2018-10-31 09:59:39 +0200

    Lisää kirjautumislogiikkaa

--- src/components/Kirjautuminen.js
+++ src/components/Kirjautuminen.js
@@ -8,50 +8,27 @@ import { Kirjaudu } from "../apiClient";
 class Kirjautuminen extends Component {
   constructor(props) {
     super(props);
-    this.state = { nimimerkki: "", salasana: "" };
+    this.state = { nimimerkki: "", salasana: "" , kayttaja: false };
   }
 
-  handleClick = event => {
-    Kirjaudu(this.state, response => {
+  handleClick = kayttaja => {
+    Kirjaudu(kayttaja, response => {
       console.log(response);
       if (typeof response.kayttaja_id !== "undefined") {
           //Kirjautuminen onnistui
+          this.setState({kayttaja: response })
       } else {
           //kirjautuminen epäonnistui
       }
     });
   };
 
+  
   render() {
+    var sisalto = !this.state.kayttaja? <Kirjautumislomake handleClick= {this.handleClick} /> : <Sisalla />;
     return (
       <div>
-        <MuiThemeProvider>
-          <div>
-            <AppBar title="Kirjautuminen" />
-            <TextField
-              hintText="Syötä nimimerkki"
-              floatingLabelText="Nimimerkki"
-              onChange={(event, uusiArvo) =>
-                this.setState({ nimimerkki: uusiArvo })
-              }
-            />
-            <br />
-            <TextField
-              hintText="Syötä salasana"
-              floatingLabelText="Salasana"
-              onChange={(event, uusiArvo) =>
-                this.setState({ salasana: uusiArvo })
-              }
-            />
-            <br />
-            <RaisedButton
-              label="Kirjaudu"
-              primary={true}
-              style={style}
-              onClick={event => this.handleClick(event)}
-            />
-          </div>
-        </MuiThemeProvider>
+        {sisalto}
       </div>
     );
   }
@@ -59,5 +36,47 @@ class Kirjautuminen extends Component {
 const style = {
   margin: 15
 };
+
+class Kirjautumislomake extends Component {
+    state = {nimimerkki: '', salasana: ''}
+    render(){
+    return(
+    <MuiThemeProvider>
+        <div>
+          <AppBar title="Kirjautuminen" />
+          <TextField
+            hintText="Syötä nimimerkki"
+            floatingLabelText="Nimimerkki"
+            onChange={(event, uusiArvo) =>
+              this.setState({ nimimerkki: uusiArvo })
+            }
+          />
+          <br />
+          <TextField
+            hintText="Syötä salasana"
+            floatingLabelText="Salasana"
+            onChange={(event, uusiArvo) =>
+              this.setState({ salasana: uusiArvo })
+            }
+          />
+          <br />
+          <RaisedButton
+            label="Kirjaudu"
+            primary={true}
+            style={style}
+            onClick={event => this.props.handleClick(this.state)}
+          />
+        </div>
+      </MuiThemeProvider>);
+      }
+}
+
+function Sisalla() {
+    return(
+        <div>
+            Moi
+        </div>
+    )
+}
 
 export default Kirjautuminen;
